aafd93ef53acbebf8fc48a87615156dc449c1c3c
nasimabo <nasimuddin555k@gmail.com>
2024-02-23 00:37:59 +0600

    relational operator

---
+++ reletional_operator.py
@@ -0,0 +1,12 @@
+#################### Relational Operator #####################
+
+
+
+num1 = int(input("Enter the number 1: "))
+num2 = int(input("Enter the number 2: "))
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 <= num2)
